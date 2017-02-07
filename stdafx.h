526956687f49b2462450049d91a477693454540c
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-07 12:49:03 +0000

    upgrade project and first build to make sure all works

--- stdafx.h
+++ stdafx.h
@@ -13,8 +13,8 @@
 #include <stdio.h>
 #include <math.h>
 
-#include <GL/glew.h>
-#include <GL/glut.h>
+#include "GL/glew.h"
+#include "GL/glut.h"
 #include "textfile.h"
 #include "globals.h"
 #include "cShader.h"

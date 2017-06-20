1e28d9b56ed25c8bc536f986bd7b78fb1be569f1
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-06-20 12:18:53 +0100

    need to find out how to interprit the face information

--- NA_ObjLoader.cpp
+++ NA_ObjLoader.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "NA_ObjLoader.h"
 
+NA_ObjLoader na_objLoader; // access with extern NA_ObjLoader na_objLoader
 
 NA_ObjLoader::NA_ObjLoader()
 {
@@ -9,4 +10,46 @@ NA_ObjLoader::NA_ObjLoader()
 
 NA_ObjLoader::~NA_ObjLoader()
 {
+}
+
+NA_Obj NA_ObjLoader::load(string fileName)
+{
+	// load file via iostream into std::vector
+
+	// seperate components based of first char of line into 3 seperate std::vectors
+
+	// pack std::vectors into NA_Obj (convert to arrays first?)
+}
+
+NA_Obj::NA_Obj()
+{
+}
+
+NA_Obj::~NA_Obj()
+{
+	if (vertex != NULL)
+	{
+		delete vertex;
+		vertex = NULL;
+		vertexCount = 0;
+	}
+	if (normal != NULL)
+	{
+		delete normal;
+		normal = NULL;
+		normalCount = 0;
+	}
+	if (face != NULL)
+	{
+		delete face;
+		face = NULL;
+		faceCount = 0;
+	}
+}
+
+NA_Tri NA_Obj::getFace(int faceID)
+{
+	//TODO: NA_Obj::getFace
+
+	return NA_Tri();
 }
--- NA_ObjLoader.h
+++ NA_ObjLoader.h
@@ -1,8 +1,38 @@
 #pragma once
+struct NA_Vec
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct NA_Tri
+{
+	NA_Vec x;
+	NA_Vec y;
+	NA_Vec z;
+};
+
+class NA_Obj
+{
+	NA_Obj();
+	~NA_Obj();
+	NA_Tri getFace(int faceID);
+	
+	int vertexCount;
+	NA_Vec* vertex;
+	int normalCount;
+	NA_Vec* normal;
+	int faceCount;
+	int* face; //how does this work?  //use array of ints to form triangles, use its to index vertex
+};
+
 class NA_ObjLoader
 {
 public:
 	NA_ObjLoader();
 	~NA_ObjLoader();
+	NA_Obj load(string fileName);
+	
 };
 

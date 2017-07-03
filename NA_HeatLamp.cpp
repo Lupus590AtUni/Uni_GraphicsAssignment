f1b24ecfedaf4b92a931274854d008310466a9a3
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-07-03 13:15:39 +0100

    move light stuff from mouse callback to NA_HeatLamp::Draw

--- NA_HeatLamp.cpp
+++ NA_HeatLamp.cpp
@@ -20,6 +20,26 @@ void NA_HeatLamp::draw()
 	glLoadIdentity();
 	glTranslatef(pos.x, pos.y, pos.z);
 	glutSolidSphere(radius, 120, 2);
+
+	extern cRenderClass graphics;
+
+	//http://www.glprogramming.com/red/chapter05.html
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light0_position[] = { graphics.mousePos.x , graphics.mousePos.y, 1.0, 0.0 };
+	GLfloat light1_position[] = { 10.0 , 0.0, 0.0, 0.0 };
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glShadeModel(GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	//glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHT1);
+
+
 	glPopMatrix();
 	
 }

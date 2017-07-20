61133245e759e01f3ccc00f7d8d6123a3d05dd85
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-07-20 15:03:53 +0100

    remove a ton of unused #includes

--- stdafx.h
+++ stdafx.h
@@ -6,12 +6,11 @@
 #pragma once
 
 
+
 #include <iostream>
 #include <tchar.h>
 #include <windows.h>
 #include <direct.h> // for getcwd
-#include <stdio.h>
-#include <math.h>
 
 #include "GL/glew.h"
 #include "GL/glut.h"
@@ -23,36 +22,18 @@
 #include "NA_HeatLamp.h"
 #include "NA_MathsLib.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <stdio.h>
-#include <iostream>
-#include <io.h>
-#include <stdlib.h>
-#include <vector>
 #include "objloader.h"
 #include "NA_Pig.h"
 
 #include <string>
-
 using std::string;
-
-using std::vector;
 
 #define GLM_FORCE_RADIANS 
 #include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp" 
-#include "glm/gtc/type_ptr.hpp"
 
 using glm::vec2;
 using glm::vec3;
 using glm::vec4;
 
-#include "NA_ObjLoader.h"
-#include <fstream>;
 
 
